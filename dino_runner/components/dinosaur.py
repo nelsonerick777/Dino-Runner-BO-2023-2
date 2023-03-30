b1b0f78c7fb85829796b948c675c0d651398472f
nelsonerick777 <Nelson.Ledezma@fundacion-jala.org>
2023-03-30 19:31:30 -0400

    Class 1

---
+++ dino_runner/components/dinosaur.py
@@ -0,0 +1,23 @@
+from dino_runner.utils.constants import RUNNING,JUMPING,DUCKING
+
+class Dinosaur:
+    
+    def __init__(self):
+        pass
+
+
+    def run(self):
+        pass
+
+
+    def duck(self):
+        pass
+
+
+    def jump(self):
+        pass
+
+
+
+
+    
